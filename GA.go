8516e79d411c36e549ee222267f4ca16c9d63520
sharath <sharathramku@umass.edu>
2018-02-05 10:22:29 -0500

    fitness function works for negative slopes now

--- GA.go
+++ GA.go
@@ -1,12 +1,28 @@
 package ga_8queens
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func StartGA(initialPopulation []string) {
 	// TODO
 	for _, t := range initialPopulation {
-		fmt.Println(fitness(t))
+		fmt.Printf("fitness for \"%s\": %d\n", t, fitness(t))
 	}
+}
+
+func addToSet(set []string, a string) bool {
+	var contains bool
+	for _, b := range set {
+		if a == b {
+			contains = true
+		}
+	}
+	if !contains {
+		set = append(set, a)
+	}
+	return !contains
 }
 
 // should return the number of non-attacking pairs of queens
@@ -16,13 +32,13 @@ func fitness(individual string) int {
 		height = append(height, int(t%48))
 	}
 	collisions := 0
-	// find height collisions
-	for i, h := range height {
-		for j, c := range height {
+	// find collisions
+	for i := 0; i < len(height); i++ {
+		for j := 0; j < len(height); j++ {
 			if i != j {
-				if h == c || (c-h) == (j-i) {
+				if height[i] == height[j] || int(math.Abs(float64(height[j]-height[i]))) == int(math.Abs(float64(j-i))) {
+					fmt.Printf("collision: (%d, %d), (%d, %d)\n", i+1, height[i], j+1, height[j])
 					collisions++
-					//fmt.Println("height")
 				}
 			}
 		}
