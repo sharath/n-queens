758cc0d2537a78ad741b68ba6077853553973db8
sharath <sharathramku@umass.edu>
2018-02-05 09:52:05 -0500

    implemented working fitness function

--- GA.go
+++ GA.go
@@ -1,7 +1,34 @@
 package ga_8queens
 
-func StartGA(initialPopulation string) {
+import "fmt"
+
+func StartGA(initialPopulation []string) {
 	// TODO
+	for _, t := range initialPopulation {
+		fmt.Println(fitness(t))
+	}
+}
+
+// should return the number of non-attacking pairs of queens
+func fitness(individual string) int {
+	var height []int
+	for _, t := range individual {
+		height = append(height, int(t%48))
+	}
+	collisions := 0
+	// find height collisions
+	for i, h := range height {
+		for j, c := range height {
+			if i != j {
+				if h == c || (c-h) == (j-i) {
+					collisions++
+					//fmt.Println("height")
+				}
+			}
+		}
+	}
+	max := len(height) * (len(height) - 1)
+	return max - collisions
 }
 
 func GA(population []string, fitness func(string) int) string {
@@ -9,11 +36,11 @@ func GA(population []string, fitness func(string) int) string {
 	return ""
 }
 
-func Reproduce(x string, y string) string {
+func Reproduce(individualX string, individualY string) string {
 	// TODO
 	return ""
 }
 
-func Mutate(x string) {
+func Mutate(individual string) {
 	// TODO
 }
