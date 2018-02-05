1ce0f5a8b85cc16b4b75019b416a3c0318ea73b2
sharath <sharathramku@umass.edu>
2018-02-05 14:00:25 -0500

    it works! (just barely)

--- GA.go
+++ GA.go
@@ -1,62 +1,107 @@
 package ga_8queens
 
 import (
-	"fmt"
 	"math"
+	"math/rand"
+	"time"
+	"fmt"
+	"strconv"
 )
 
-func StartGA(initialPopulation []string) {
-	// TODO
-	for _, t := range initialPopulation {
-		fmt.Printf("fitness for \"%s\": %d\n", t, fitness(t))
-	}
-}
+var size int
 
-func addToSet(set []string, a string) bool {
-	var contains bool
-	for _, b := range set {
-		if a == b {
-			contains = true
+func StartGA(initialPopulation []string, iterations int, s int) {
+	size = s
+	fmt.Println("Started GA. This might take a while...")
+	population := initialPopulation
+	var solved bool
+	var solution string
+	var i int
+	for !solved {
+		i++
+		population = GAIteration(population)
+		for _, t := range population {
+			if Fitness(t) == (size * (size - 1)) {
+				solution = t
+				solved = true
+			}
 		}
 	}
-	if !contains {
-		set = append(set, a)
-	}
-	return !contains
+	fmt.Printf("Solution: \"%s\" in %d Iterations\n", solution, i)
 }
 
 // should return the number of non-attacking pairs of queens
-func fitness(individual string) int {
+func Fitness(individual string) int {
 	var height []int
-	for _, t := range individual {
-		height = append(height, int(t%48))
+	for _, c := range []byte(individual) {
+		height = append(height, int(c%48))
 	}
 	collisions := 0
 	// find collisions
 	for i := 0; i < len(height); i++ {
 		for j := 0; j < len(height); j++ {
 			if i != j {
-				if height[i] == height[j] || int(math.Abs(float64(height[j]-height[i]))) == int(math.Abs(float64(j-i))) {
-					fmt.Printf("collision: (%d, %d), (%d, %d)\n", i+1, height[i], j+1, height[j])
+				// if height is same or if the absolute value of the slope is equal to 1, there exists a collision
+				if height[i] == height[j] || math.Pow(float64(height[j]-height[i]), 2) == math.Pow(float64(j-i), 2) {
 					collisions++
 				}
 			}
 		}
 	}
-	max := len(height) * (len(height) - 1)
-	return max - collisions
+	return (size * (size - 1)) - collisions
 }
 
-func GA(population []string, fitness func(string) int) string {
-	// TODO
-	return ""
+// choose randomly from population favoring fit individuals
+func RandomSelection(population []string) string {
+	/*// TODO: favor more fit individuals
+	a := population[rand.Int()%(len(population)-1)]
+	b := population[rand.Int()%(len(population)-1)]
+	if Fitness(a) > Fitness(b) {
+		return a
+	} else {
+		return b
+	}*/
+	return population[rand.Int()%(len(population)-1)]
 }
 
-func Reproduce(individualX string, individualY string) string {
-	// TODO
-	return ""
+func GAIteration(population []string) []string {
+	var newPopulation []string
+	threshold := 0.4
+	for i := 0; i < len(population); i++ {
+		x := RandomSelection(population)
+		y := RandomSelection(population)
+		child1, child2 := Reproduce(x, y)
+		child1 = Mutate(child1, threshold)
+		child2 = Mutate(child2, threshold)
+		newPopulation = append(newPopulation, child1)
+		newPopulation = append(newPopulation, child2)
+	}
+	return newPopulation
 }
 
-func Mutate(individual string) {
-	// TODO
+//generate a random number c from 1 to len(x) and split into substrings
+func Reproduce(individualX string, individualY string) (string, string) {
+	rand.Seed(int64(time.Now().Nanosecond()))
+	r := rand.Int() % len(individualX)
+	n := (r * r) % len(individualX)
+	return individualX[n:] + individualY[:n], individualY[n:] + individualX[:n]
+}
+
+func Mutate(c string, threshold float64) string {
+	rand.Seed(int64(time.Now().Nanosecond()))
+	if float64(rand.Uint64()%10000)/10000.0 < threshold {
+		r := strconv.Itoa(rand.Int()%size + 1)
+		i := rand.Int() % size
+		var n string
+		for j, ch := range c {
+			if j != i {
+				n += strconv.Itoa(int(ch % 48))
+			} else {
+				n += r
+			}
+		}
+		return n
+	} else {
+		return c
+	}
 }
